4cbffaa761ff8fadd46cfa14e462fcefb2281da5
avamunch360-hub <ava.munch360@students.cmcss.net>
2025-12-18 08:49:25 -0600

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,1 +1,15 @@
+# Week 1: Chores and first purchase
+alowence = dishes
+alowence = lawn
+alowence = candy
 
+# Week 2: Bonus week and purchase
+alowence *= 3
+alowence = vacuum
+alowence = toy
+
+# Wee
+savings = alowence/0.5
+
+# Print final allowance
+print(f"Allowance: $43.0")
